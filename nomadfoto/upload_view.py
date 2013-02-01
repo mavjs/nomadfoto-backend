92e7de017a1aac1424ba96fef940dc788aea1444
Maverick JS <mavjs01@gmail.com>
2013-02-01 17:54:34 +0800

    updated upload view

--- nomadfoto/upload_view.py
+++ nomadfoto/upload_view.py
@@ -1,11 +1,20 @@
+from pyramid.security import Allow
 from pyramid.view import view_config
 from pyramid_deform import FormView
 from pyramid.httpexceptions import HTTPFound
-from .myschema import UploadSchema
-from .models import ImageStore
-from .uploadcheck import CheckFileType
+#from .uploadcheck import CheckFileType
 from cStringIO import StringIO
+from .models import ImageStore
+from .myschema import UploadSchema
+from . import get_user
+import mimetypes
+import os
+import zipfile
+import hashlib
 
+choices = [
+                ('', '-- Select --')
+                ]
 @view_config(
         name="upload",
         permission="add_upload",
@@ -14,21 +23,36 @@ class Upload(FormView):
     schema = UploadSchema()
     buttons = ('upload',)
     title = u"Upload"
+
     
     def upload_success(self, appstruct):
+        username = appstruct['username']
+        user = get_user(self.request, username)
+        if user is None:
+            self.request.session.flash(
+                    u"No such users exist", "error")
+            return None
         images = appstruct['items']
         image_files = images['fp'].read()
         image_type = images['mimetype']
-        images_dict = {}
         if image_type == 'application/zip':
-            images_dict = CheckFileType(StringIO(image_files)).unzip()
-        elif image_type == 'application/x-tar':
-            images_dict = CheckFileType(StringIO(image_files)).untar()
-        for key,value in images_dict.items():
-            image_uid = str(key)
-            key = ImageStore(
-                    image_file=value,
-                    )
-            self.request.root['images'][image_uid] = key
+            zip_list = zipfile.ZipFile(StringIO(image_files), 'r')
+            for item in zip_list.namelist():
+                if not os.path.basename(item):
+                    continue
+                split_name = item.split('/')[-1] 
+                file_uid = str(hashlib.md5(split_name).hexdigest())
+                unzip_image = zip_list.read(item).encode('base64')
+                finished_image = "data:"+mimetypes.guess_type(item)[0]+";base64,"+unzip_image
+                source = ImageStore(
+                        uid=file_uid,
+                        image_name=split_name,
+                        image_file=finished_image,
+                        )
+                source.__acl__ = [
+                        (Allow, user.title, 'view'),
+                        (Allow, user.title, 'share'),
+                        ]
+                self.request.root['images'][file_uid] = source
         self.request.session.flash(u"Your folder was added.", "success")
         return HTTPFound(location=self.request.application_url)
