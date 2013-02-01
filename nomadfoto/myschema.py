849f55f5cb3fd671570e402507f9b2271cf59a18
Maverick JS <mavjs01@gmail.com>
2013-02-01 17:51:55 +0800

    updated schema

--- nomadfoto/myschema.py
+++ nomadfoto/myschema.py
@@ -1,13 +1,8 @@
 from colander import Schema, SchemaNode, String
+from deform.widget import PasswordWidget
+import colander
 from deform import FileData
-from deform.widget import PasswordWidget, FileUploadWidget
-from deform.interfaces import FileUploadTempStore
-
-class Store(dict):
-    def preview_url(self, name):
-        return ""
-
-store = Store()
+from deform.widget import FileUploadWidget, SelectWidget
 
 class LoginSchema(Schema):
     username = SchemaNode(String(), title=u"username")
@@ -17,6 +12,17 @@ class RegistrationSchema(LoginSchema):
     fullname = SchemaNode(String(), title=u"Full Name")
     email = SchemaNode(String(), title=u"Email")
 
+class Store(dict):
+    def preview_url(self, name):
+        return ""
+
+store = Store()
+
+@colander.deferred
+def deferred_users_validator(node, kw):
+    usernames = kw.get('usernames', [])
+    return SelectWidget(values=usernames)
+
 class UploadSchema(Schema):
-    description = SchemaNode(String(), title=u"Description for files")
+    username = SchemaNode(String(), title=u"Username")
     items = SchemaNode(FileData(), widget=FileUploadWidget(store))
