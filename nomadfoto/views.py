5408e8e2926edbc5863e2d8bde3481da691001b7
Maverick JS <mavjs01@gmail.com>
2013-02-08 19:16:05 +0800

    add proper redirect for users when accessing main site

--- nomadfoto/views.py
+++ nomadfoto/views.py
@@ -1,10 +1,16 @@
 from pyramid.view import view_config
 from .models import User
+from pyramid.security import authenticated_userid
+from pyramid.httpexceptions import HTTPFound
 
 @view_config(
         renderer="templates/index.pt")
 def index(request):
-    return {}
+    authd_user = authenticated_userid(request)
+    if not authd_user or authd_user == 'admin':
+        return {}
+    else:
+        return HTTPFound(location=request.resource_url(request.user))
 
 @view_config(
         context=User,
