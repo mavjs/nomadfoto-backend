ccc638f2d834fb587d2a7489c23f02d5213dd06c
Maverick JS <mavjs01@gmail.com>
2013-03-11 18:08:26 +0800

    added pagination for collections

--- nomadfoto/jobs_view.py
+++ nomadfoto/jobs_view.py
@@ -1,5 +1,7 @@
 from pyramid.view import view_config
+from webhelpers.paginate import Page
 from .models import User
+from webhelpers.paginate import PageURL_WebOb, Page
 
 @view_config(
         context=User,
@@ -13,5 +15,16 @@ def jobs_view(context, request):
         context=User,
         permission="users",
         renderer="templates/user.pt")
-def user_view(context, request):
-    return {}
+def user_view(request):
+    if not "page" in request.params:
+        next_page=1
+    else:
+        next_page = request.params["page"]
+    collection = request.root['images'].values()
+    url_for_page = PageURL_WebOb(request)
+    images = Page(collection,
+            page=next_page,
+            items_per_page=30,
+            url=url_for_page
+            )
+    return {"images" : images}
--- nomadfoto/views.py
+++ nomadfoto/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 from pyramid.security import authenticated_userid
 from pyramid.httpexceptions import HTTPFound
+from webhelpers.paginate import PageURL_WebOb, Page
 
 @view_config(
         renderer="templates/index.pt")
@@ -10,3 +11,26 @@ def index(request):
         return {}
     else:
         return HTTPFound(location=request.resource_url(request.user))
+
+@view_config(
+        name="pricelist",
+        renderer="templates/price.pt")
+def pricelist(request):
+    return {}
+
+@view_config(
+        name="collections",
+        renderer="templates/collections.pt")
+def collections(context, request):
+    if not "page" in request.params:
+        next_page=1
+    else:
+        next_page = request.params["page"]
+    collection = request.root['images'].values()
+    url_for_page = PageURL_WebOb(request)
+    images = Page(collection,
+            page=next_page,
+            items_per_page=30,
+            url=url_for_page
+            )
+    return {"images" : images}
